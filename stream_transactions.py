916a7c9233bd80c51d300adbd02b8c9b246a1ff1
joliezahra12 <22611009@students.uii.ac.id>
2024-07-22 03:16:03 +0700

    Update stream_transactions.py

--- stream_transactions.py
+++ stream_transactions.py
@@ -7,11 +7,8 @@ diabetes_model = pickle.load(open('upitrans_model.sav', 'rb'))
 # Judul web
 st.title('Prediksi Status')
 
-# Membagi kolom
-col1 = st.columns(1)
-
-with col1:
-    amount_inr = st.text_input('Input Amount (INR)')
+# Membuat input
+amount_inr = st.text_input('Input Amount (INR)')
 
 # Code untuk prediksi
 upitrans_pred = ''
@@ -22,14 +19,7 @@ if st.button('Test Prediksi Status'):
         amount_inr = float(amount_inr)  # Konversi input menjadi float
         upitrans_pred = diabetes_model.predict([[amount_inr]])
 
-        # Memetakan hasil prediksi
-        if upitrans_pred == 'FAILED':
-            upitrans_pred = 0
-        elif upitrans_pred == 'SUCCESS':
-            upitrans_pred = 1
-        else:
-            upitrans_pred = None
-
-        st.success(f'Prediksi Status: {upitrans_pred}')
+        # Menampilkan hasil prediksi
+        st.success(f'Prediksi Status: {upitrans_pred[0]}')
     except ValueError:
         st.error('Input Amount (INR) harus berupa angka.')
