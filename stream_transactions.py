28d47187130bb08be1479a802b2fda4b6c90a021
joliezahra12 <22611009@students.uii.ac.id>
2024-07-22 03:14:00 +0700

    Update stream_transactions.py

--- stream_transactions.py
+++ stream_transactions.py
@@ -11,21 +11,25 @@ st.title('Prediksi Status')
 col1 = st.columns(1)
 
 with col1:
-    Amount (INR) = st.text_input('Input Amount (INR)')
+    amount_inr = st.text_input('Input Amount (INR)')
 
 # Code untuk prediksi
 upitrans_pred = ''
 
 # Membuat tombol untuk prediksi
 if st.button('Test Prediksi Status'):
-    upitrans_pred = diabetes_model.predict([[Amount (INR)]])
+    try:
+        amount_inr = float(amount_inr)  # Konversi input menjadi float
+        upitrans_pred = diabetes_model.predict([[amount_inr]])
 
-    # Memetakan hasil prediksi
-    if upitrans_pred == 'FAILED':
-        upitrans_pred = 0
-    elif upitrans_pred == 'SUCCESS':
-        upitrans_pred = 1
-    else:
-        upitrans_pred = None
+        # Memetakan hasil prediksi
+        if upitrans_pred == 'FAILED':
+            upitrans_pred = 0
+        elif upitrans_pred == 'SUCCESS':
+            upitrans_pred = 1
+        else:
+            upitrans_pred = None
 
-    st.success(f'Prediksi Status: {upitrans_pred}')
+        st.success(f'Prediksi Status: {upitrans_pred}')
+    except ValueError:
+        st.error('Input Amount (INR) harus berupa angka.')
